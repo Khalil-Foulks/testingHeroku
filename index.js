8657b267f6136afe3bb8823ed71d26703f2b4b8b
Khalil <khalilfoulks@gmail.com>
2020-08-06 13:14:00 -0400

    added default port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const server = express()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT|| 4000
 
 server.get('/', function(req, res) {
     res.status(200).json({message: `Success!`})
